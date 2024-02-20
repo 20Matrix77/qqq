a1f044c8fc7b550f5abd9a68b5334be454464e78
20Matrix77 <101499676+20Matrix77@users.noreply.github.com>
2024-02-20 23:43:01 +0500

    Update vpn.py

--- vpn.py
+++ vpn.py
@@ -9,7 +9,7 @@ bot = commands.Bot(command_prefix='>', intents=intents)
 
 @bot.event
 async def on_ready():
-    channel = bot.get_channel(INSERT_YOUR_CHANNEL_ID)
+    channel = bot.get_channel(1208155463795867648)
     await channel.send(f"```Linux-> Б0т п0дключен```")
 
 bot.run('MTIwODEzMjEyODU2NzU5MTAwMw.G7SIu8.nHYc9en0jM_RijBrAO6Ecv-ivvkh9w7_1X2cNs')
