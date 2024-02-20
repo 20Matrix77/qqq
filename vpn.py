d258b0c52dcd5a4b9edb9e8b2d352d55670dc2de
20Matrix77 <101499676+20Matrix77@users.noreply.github.com>
2024-02-20 23:42:35 +0500

    Update vpn.py

--- vpn.py
+++ vpn.py
@@ -2,11 +2,6 @@ import discord
 from discord.ext import commands
 
 intents = discord.Intents.default()
-
-intents.members = True 
-intents.message_content = True
-intents.messages = True
-intents.dm_messages = True
 intents.typing = False
 intents.presences = False
 
@@ -14,7 +9,7 @@ bot = commands.Bot(command_prefix='>', intents=intents)
 
 @bot.event
 async def on_ready():
-    channel = bot.get_channel(1208155463795867648)
-    await channel.send(f"```Linux-> B0t c0nnected```")
+    channel = bot.get_channel(INSERT_YOUR_CHANNEL_ID)
+    await channel.send(f"```Linux-> Б0т п0дключен```")
 
 bot.run('MTIwODEzMjEyODU2NzU5MTAwMw.G7SIu8.nHYc9en0jM_RijBrAO6Ecv-ivvkh9w7_1X2cNs')
